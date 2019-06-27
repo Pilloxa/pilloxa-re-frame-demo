eefb92f5adbfc3d87cec53b44ad36329816fcc34
Niclas Nilsson <niclas.love.nilsson@gmail.com>
2019-06-27 14:45:29 +0200

    Don't reload app state on reload

--- src/pilloxa_re_frame_demo/core.cljs
+++ src/pilloxa_re_frame_demo/core.cljs
@@ -1,6 +1,10 @@
 (ns ^:figwheel-hooks pilloxa-re-frame-demo.core
   (:require [reagent.core :as reagent]
-            [re-frame.core :as rf]
+            [re-frame.core :refer [dispatch
+                                   dispatch-sync
+                                   reg-event-db
+                                   reg-sub
+                                   subscribe]]
             [cljsjs.react-chartjs-2]))
 
 (def line-chart
@@ -9,36 +13,36 @@
 (def initial-state
   {:date-range ["2019-06-01" "2019-06-02" "2019-06-03" "2019-06-04"]
    :patients   {:patient-1 {:id           :patient-1
-                            :dose-history [80 82 85 82]
+                            :dose-history [(rand-int 100) (rand-int 100) (rand-int 100) (rand-int 100)]
                             :color        "rgb(255, 0, 0)"
-                            :selected?    false}
+                            :selected?    true}
                 :patient-2 {:id           :patient-2
                             :dose-history [92 93 95 95]
                             :color        "rgb(0, 0, 255)"
                             :selected?    true}}})
 
-(rf/reg-event-db
+(reg-event-db
   :initialize
   (fn [_ _]
     initial-state))
 
-(rf/reg-event-db
+(reg-event-db
   :toggle-select
   (fn [db [_ patient-id]]
     (update-in db [:patients patient-id :selected?] not)))
 
-(rf/reg-sub
+(reg-sub
   :patients
   (fn [db _]
     (vals (:patients db))))
 
-(rf/reg-sub
+(reg-sub
   :selected-patients
   :<- [:patients]
   (fn [patients]
     (filter :selected? patients)))
 
-(rf/reg-sub
+(reg-sub
   :date-range
   (fn [db _]
     (:date-range db)))
@@ -47,15 +51,15 @@
   [:div {:style {:display        :flex
                  :flex-direction :row}}
    [:input {:type     :checkbox
-            :checked selected?
-            :onChange #(rf/dispatch [:toggle-select id])}]
+            :checked  selected?
+            :onChange #(dispatch [:toggle-select id])}]
    [:div {:style {:width            20
                   :height           20
                   :background-color color}}]
    [:div id]])
 
 (defn patient-selectors []
-  (let [patients (rf/subscribe [:patients])]
+  (let [patients (subscribe [:patients])]
     [:div {:style {:display        :flex
                    :flex-direction :column}}
      (for [patient @patients]
@@ -63,20 +67,20 @@
        [patient-selector patient])]))
 
 (defn dose-chart []
-  (let [date-range        (rf/subscribe [:date-range])
-        selected-patients (rf/subscribe [:selected-patients])]
+  (let [date-range        (subscribe [:date-range])
+        selected-patients (subscribe [:selected-patients])]
     [:div {:style {:width  400
                    :height 300}}
      [line-chart
       {:legend {:display false}
-       :data {:labels   @date-range
-              :datasets (for [{:keys [id color dose-history]} @selected-patients]
-                          {:label           id
-                           :fill            false
-                           :lineTension     0.1
-                           :backgroundColor color
-                           :borderColor     color
-                           :data            dose-history})}}]]))
+       :data   {:labels   @date-range
+                :datasets (for [{:keys [id color dose-history]} @selected-patients]
+                            {:label           id
+                             :fill            false
+                             :lineTension     0.1
+                             :backgroundColor color
+                             :borderColor     color
+                             :data            dose-history})}}]]))
 
 (defn app-root []
   (let []
@@ -88,11 +92,15 @@
       [patient-selectors]
       [dose-chart]]]))
 
+(defonce reload-cnt
+  (atom 0))
+
 (defn main []
-  (rf/dispatch-sync [:initialize])
+  (when (zero? @reload-cnt)
+    (dispatch-sync [:initialize]))
   (reagent/render [app-root] (js/document.getElementById "app")))
 
 (defn ^:after-load on-reload []
-  #_(main))
+  (swap! reload-cnt inc))
 
 (main)
